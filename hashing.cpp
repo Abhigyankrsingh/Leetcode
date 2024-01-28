82e5141388614f7407217dd5140de06a634464af
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2024-01-28 14:24:59 +0530

    Code_Updated

--- hashing.cpp
+++ hashing.cpp
@@ -1,36 +1,34 @@
 #include<bits/stdc++.h>
 
 using namespace std;
-
-
 int main(){
 
-    int n; 
-    cin >>n;
-
-    int arr[n];
-
+    int n;
+    cin >> n;
+     
+    int  arr[n];
     for(int i =0; i<n;i++){
-  
-     cin >> arr[i];
-   
+        cin >> arr[i];
     }
 
-    map<int,int> mpp;
+    map<int, int>mpp;
 
     for(int i =0; i<n;i++){
         mpp[arr[i]]++;
     }
 
 
-  int q;
+    int q;
 
-  cin >>q;
-    
- while(q--){
-    int number;
+    cin >> q;
 
-    cin>> number;
-    cout << mpp[number] <<endl;
- }
+    while(q--){
+     int number;
+
+     cin >> number;
+
+     cout << mpp[number]<< endl;
+    }
+
+    return 0;
 }
